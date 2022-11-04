19cee3ad97402168a236dcfd083832a330128c25
cgunn92 <nominetrinitatis@gmail.com>
2022-11-03 19:09:03 -0700

    Make ui functional

--- rps.js
+++ rps.js
@@ -1,3 +1,25 @@
+const win = document.querySelector('p.winCount');
+const lose = document.querySelector('p.loseCount');
+const tie = document.querySelector('p.tieCount');
+const content = document.querySelector('div.content');
+
+const paper = document.querySelector('button.paper');
+const rock = document.querySelector('button.rock');
+const scissors = document.querySelector('button.scissors');
+
+paper.addEventListener('click', getClassName);
+rock.addEventListener('click', getClassName);
+scissors.addEventListener('click', getClassName);
+
+function getClassName(e){
+    let input = e.target.className;
+    console.log(input);
+    console.log(e);
+    keepScore(playRound(input, getComputerChoice()));
+    endGame();
+}
+
+
 function getComputerChoice(){
     let rock = 'rock';
     let paper = 'paper';
@@ -16,69 +38,89 @@ function getComputerChoice(){
     }
 }
 
-/*
-function getUserChoice(){
-    let userInput;
 
-    while(true){
-        userInput = prompt("Choose between Rock, Paper, or Scissors: ").toLowerCase();
-        if(userInput === 'rock' || userInput === 'paper' || userInput === 'scissors'){
-            return userInput;
-        }
-        else{
-            console.log("\nThat was not an acceptable input, try again.\n")
-        } 
+function keepScore(winner){
+    if(winner === 'player'){
+        win.textContent = parseInt(win.textContent) + 1;
     }
-    
+    else if(winner === 'computer'){
+        lose.textContent = parseInt(lose.textContent) + 1;
+    }
+    else{
+        tie.textContent = parseInt(tie.textContent) + 1;
+    }
 }
-*/
+
 
 function playRound(playerSelection, computerSelection){
-    computerSelection = getComputerChoice();
-    playerSelection = getUserChoice();
     const s = 'scissors';
     const r = 'rock';
     const p = 'paper';
     const play = 'player';
     const comp = 'computer';
     const tie = 'tie';
-    var winner;
+    let winner;
 
-    if(playerSelection == r){ //player chooses rock
-        if(computerSelection == p){
+    if(playerSelection === r){ //player chooses rock
+        if(computerSelection === p){
             winner = comp;
         }
-        else if(computerSelection == s){
+        else if(computerSelection === s){
             winner = play;
         }
         else{
             winner = tie;
         }
     }
-    else if(playerSelection == p){ //player chooses paper
-        if(computerSelection == r){
+    else if(playerSelection === p){ //player chooses paper
+        if(computerSelection === r){
             winner = play;
         }
-        else if(computerSelection == s){
+        else if(computerSelection === s){
             winner = comp;
         }
         else{
             winner = tie;
         }
     }
-    else if(playerSelection == s){ //player choosees scissors
-        if(computerSelection == p){
+    else if(playerSelection === s){ //player choosees scissors
+        if(computerSelection === p){
             winner = play;
         }
-        else if(computerSelection == r){
+        else if(computerSelection === r){
             winner = comp;
         }
         else{
             winner = tie;
         }
     }
+
+    console.log(winner);
 
     return winner;
+}
+
+function endGame(){
+    let wins = parseInt(win.textContent);
+    let losses = parseInt(lose.textContent);
+    let ties = parseInt(tie.textContent);
+
+    let count = wins + losses + ties;
+
+    if(count === 5){
+        if(wins > losses){
+            results.textContent = 'You won!';
+            content.appendChild(results);
+        }
+        else if(losses > wins){
+            results.textContent = 'You lost!';
+            content.appendChild(results);
+        }
+        else if(ties === 5){
+            results.textContent = 'Tie!';
+            content.appendChild(results);
+        }
+    }
 }
 
 
@@ -124,4 +166,7 @@ function playGame(){
     }
 }
 
-playGame();
+const results = document.createElement('div');
+
+//playRound();
+//playGame();
